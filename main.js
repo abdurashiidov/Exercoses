2cfaa6ecbc1d24d98ac7a3fe391f3fca0b182c68
abdurashiidov <abdurashiidov18@gmail.com>
2022-08-09 01:28:15 +0300

    commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//! 1-Exercise
+// let a = 10;
+// let b = 5;
+// console.log(a+b);
+
+//! 2-Exercise
+// let a = 25;
+// let b = 15;
+// let result = a+b;
+// console.log(result);
+
+//! 3-Exercise
+// let a = 10;
+// let b = 5;
+// let c = a-b;
+// let d = 7;
+// let result = a + d;
+// console.log(result);
+
+//! 4-Exercise
+// let a = 10;
+// let b = 3;
+// console.log(a % b);
+
+//! 5-Exercise
+let a = 2;
+console.log(a ** 10);
+
